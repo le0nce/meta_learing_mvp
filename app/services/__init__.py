37be64e30c4f19abe50578745bacbf39c7d9674e
Leon Rieger <leon-rieger.1@gmx.de>
2025-07-19 08:46:07 +0000

    Removed lobster service as python version needs to be changed for tensorflow. Can only be done on windows machine

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,6 +3,5 @@
 from app.services.knowledge_builder_service import KnowledgeBuilderService
 from app.services.console_protocol import ConsoleProtocol
 from app.services.open_ml_service import OpenMLService
-from app.services.lobster_service import LobsterService
 
-__all__ = ["KnowledgeBuilderService", "ConsoleProtocol", "OpenMLService", "LobsterService"]
+__all__ = ["KnowledgeBuilderService", "ConsoleProtocol", "OpenMLService"]
